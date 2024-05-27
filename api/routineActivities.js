1252078622b0f5d1cb8d18b41ad72f6d6cb00280
devin01678 <devingsaucier@gmail.com>
2024-05-27 11:48:03 -0500

    final submit

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -1,45 +1,124 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { updateRoutineActivity, canEditRoutineActivity, destroyRoutineActivity, getRoutineActivityById } = require('../db');
-const client = require('../db/client');
-const { requireUser, requiredNotSent } = require('./utils')
+const {
+  updateRoutineActivity,
+  canEditRoutineActivity,
+  destroyRoutineActivity,
+  getRoutineActivityById,
+  getAllRoutineActivities,
+  createRoutineActivity,
+  getRoutineActivityByRoutineId,
+} = require("../db");
+const client = require("../db/client");
+const { requireUser, requiredNotSent } = require("./utils");
 
-
-
-// PATCH /api/routine_activities/:routineActivityId
-router.patch('/:routineActivityId', requireUser, requiredNotSent({requiredParams: ['count', 'duration'], atLeastOne: true}), async (req, res, next) => {
+// GET /api/routines
+router.get("/", async (req, res, next) => {
   try {
-    const {count, duration} = req.body;
-    const {routineActivityId} = req.params;
-    const routineActivityToUpdate = await getRoutineActivityById(routineActivityId);
-    if(!routineActivityToUpdate) {
-      next({
-        name: 'NotFound',
-        message: `No routine_activity found by ID ${routineActivityId}`
-      })
-    } else {
-      if(!await canEditRoutineActivity(req.params.routineActivityId, req.user.id)) {
-        res.status(403);
-        next({name: "Unauthorized", message: "You cannot edit this routine_activity!"});
-      } else {
-        const updatedRoutineActivity = await updateRoutineActivity({id: req.params.routineActivityId, count, duration})
-        res.send(updatedRoutineActivity);
-      }
-    }
+    // TODO - send back all data, including private, if token present. This would mean adding only the data for the user that matches the request
+    const routine_activities = await getAllRoutineActivities();
+    res.send(routine_activities);
   } catch (error) {
     next(error);
   }
 });
 
+// POST /api/routine_activities
+router.post("/");
+requireUser,
+  requiredNotSent({
+    requiredParams: ["routineId", "activityId", "duration", "count"],
+  }),
+  async (req, res, next) => {
+    try {
+      const { routineId, activityId, duration, count } = req.body;
+      const existingActivity = await getRoutineActivityByRoutineId(routineId);
+      if (existingActivity) {
+        next({
+          name: "NotFound",
+          message: `An activity with routineId ${routineId} already exists`,
+        });
+      } else {
+        const createdActivity = await createRoutineActivity({
+          routineId,
+          activityId,
+          duration,
+          count,
+        });
+        if (createdActivity) {
+          res.send(createdActivity);
+        } else {
+          next({
+            name: "FailedToCreate",
+            message: "There was an error creating your activity",
+          });
+        }
+      }
+    } catch (error) {
+      next(error);
+    }
+  };
+
+// PATCH /api/routine_activities/:routineActivityId
+router.patch(
+  "/:routineActivityId",
+  requireUser,
+  requiredNotSent({ requiredParams: ["count", "duration"], atLeastOne: true }),
+  async (req, res, next) => {
+    try {
+      const { count, duration } = req.body;
+      const { routineActivityId } = req.params;
+      const routineActivityToUpdate = await getRoutineActivityById(
+        routineActivityId
+      );
+      if (!routineActivityToUpdate) {
+        next({
+          name: "NotFound",
+          message: `No routine_activity found by ID ${routineActivityId}`,
+        });
+      } else {
+        if (
+          !(await canEditRoutineActivity(
+            req.params.routineActivityId,
+            req.user.id
+          ))
+        ) {
+          res.status(403);
+          next({
+            name: "Unauthorized",
+            message: "You cannot edit this routine_activity!",
+          });
+        } else {
+          const updatedRoutineActivity = await updateRoutineActivity({
+            id: req.params.routineActivityId,
+            count,
+            duration,
+          });
+          res.send(updatedRoutineActivity);
+        }
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
 // DELETE /api/routine_activities/:routineActivityId
-router.delete('/:routineActivityId', requireUser, async (req, res, next) => {
+router.delete("/:routineActivityId", requireUser, async (req, res, next) => {
   try {
-    if(!await canEditRoutineActivity(req.params.routineActivityId, req.user.id)) {
+    if (
+      !(await canEditRoutineActivity(req.params.routineActivityId, req.user.id))
+    ) {
       res.status(403);
-      next({name: "Unauthorized", message: "You cannot edit this routine_activity!"});
+      next({
+        name: "Unauthorized",
+        message: "You cannot edit this routine_activity!",
+      });
     } else {
-      const deletedRoutineActivity = await destroyRoutineActivity(req.params.routineActivityId)
-      res.send({success: true, ...deletedRoutineActivity});
+      const deletedRoutineActivity = await destroyRoutineActivity(
+        req.params.routineActivityId
+      );
+      res.send({ success: true, ...deletedRoutineActivity });
     }
   } catch (error) {
     next(error);
--- db/activities.js
+++ db/activities.js
@@ -1,34 +1,46 @@
-const client = require('./client');
-const util = require('./util');
+const client = require("./client");
+const util = require("./util");
+const { attachActivitiesToActivities } = require("./activities");
 
 // database functions
-async function getAllActivities(){
+async function getAllActivities() {
   try {
-    const {rows} = await client.query(`
-      SELECT * FROM activities;
-    `);
-    return rows;
+    const { rows: activities } = await client.query(
+      // Here we are fetching the videoGames table from the database and displaying it onto the screen
+      "SELECT * from activities;"
+    );
+    return activities;
   } catch (error) {
     throw error;
   }
 }
-async function getActivityById(id){
+async function getActivityById(id) {
   try {
-    const {rows: [activity]} = await client.query(`
+    const {
+      rows: [activity],
+    } = await client.query(
+      `
       SELECT * FROM activities
       WHERE id = $1
-    `, [id]);
+    `,
+      [id]
+    );
     return activity;
   } catch (error) {
     throw error;
   }
 }
-async function getActivityByName(name){
+async function getActivityByName(name) {
   try {
-    const {rows: [activity]} = await client.query(`
+    const {
+      rows: [activity],
+    } = await client.query(
+      `
       SELECT * FROM activities
       WHERE name = $1
-    `, [name]);
+    `,
+      [name]
+    );
     return activity;
   } catch (error) {
     throw error;
@@ -36,13 +48,16 @@ async function getActivityByName(name){
 }
 async function getActivitiesByRoutineId(id) {
   try {
-    const { rows: activities } = await client.query(`
+    const { rows: activities } = await client.query(
+      `
     SELECT activities.*, routine_activities.duration, routine_activities.count, routine_activities.id AS "routineActivityId"
     FROM activities 
     JOIN routine_activities ON routine_activities."activityId" = activities.id
     WHERE routine_activities."routineId" = $1;
-  `, [id]);
-  return activities;
+  `,
+      [id]
+    );
+    return activities;
   } catch (error) {
     throw error;
   }
@@ -50,23 +65,28 @@ async function getActivitiesByRoutineId(id) {
 async function attachActivitiesToRoutines(routines) {
   // no side effects
   const routinesToReturn = [...routines];
-  const binds = routines.map((_, index) => `$${index + 1}`).join(', ');
-  const routineIds = routines.map(routine => routine.id);
+  const binds = routines.map((_, index) => `$${index + 1}`).join(", ");
+  const routineIds = routines.map((routine) => routine.id);
   if (!routineIds?.length) return [];
-  
+
   try {
     // get the activities, JOIN with routine_activities (so we can get a routineId), and only those that have those routine ids on the routine_activities join
-    const { rows: activities } = await client.query(`
+    const { rows: activities } = await client.query(
+      `
       SELECT activities.*, routine_activities.duration, routine_activities.count, routine_activities.id AS "routineActivityId", routine_activities."routineId"
       FROM activities 
       JOIN routine_activities ON routine_activities."activityId" = activities.id
-      WHERE routine_activities."routineId" IN (${ binds });
-    `, routineIds);
+      WHERE routine_activities."routineId" IN (${binds});
+    `,
+      routineIds
+    );
 
     // loop over the routines
-    for(const routine of routinesToReturn) {
+    for (const routine of routinesToReturn) {
       // filter the activities to only include those that have this routineId
-      const activitiesToAdd = activities.filter(activity => activity.routineId === routine.id);
+      const activitiesToAdd = activities.filter(
+        (activity) => activity.routineId === routine.id
+      );
       // attach the activities to each single routine
       routine.activities = activitiesToAdd;
     }
@@ -77,37 +97,46 @@ async function attachActivitiesToRoutines(routines) {
 }
 
 // select and return an array of all activities
-async function createActivity({ name, description }){
+async function createActivity({ name, description }) {
   try {
-    const {rows: [activity]} = await client.query(`
+    const {
+      rows: [activity],
+    } = await client.query(
+      `
       INSERT INTO activities(name, description) VALUES ($1, $2)
       ON CONFLICT (name) DO NOTHING 
       RETURNING *
-    `, [name, description]);
+    `,
+      [name, description]
+    );
     return activity;
   } catch (error) {
     throw error;
-  }}
+  }
+}
 // return the new activity
-async function updateActivity({id, ...fields}){
+async function updateActivity({ id, ...fields }) {
   try {
-    const toUpdate = {}
-    for(let column in fields) {
-      if(fields[column] !== undefined) toUpdate[column] = fields[column];
+    const toUpdate = {};
+    for (let column in fields) {
+      if (fields[column] !== undefined) toUpdate[column] = fields[column];
     }
     let activity;
     if (util.dbFields(toUpdate).insert.length > 0) {
-      const {rows} = await client.query(`
+      const { rows } = await client.query(
+        `
         UPDATE activities
-        SET ${ util.dbFields(toUpdate).insert }
-        WHERE id=${ id }
+        SET ${util.dbFields(toUpdate).insert}
+        WHERE id=${id}
         RETURNING *;
-      `, Object.values(toUpdate));
+      `,
+        Object.values(toUpdate)
+      );
       activity = rows[0];
     }
     return activity;
   } catch (error) {
-    throw error
+    throw error;
   }
 }
 // don't try to update the id
@@ -121,4 +150,4 @@ module.exports = {
   attachActivitiesToRoutines,
   createActivity,
   updateActivity,
-}
+};
